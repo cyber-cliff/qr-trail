b52430dc99bdf703fcd25d24d2f22cd511d47605
Wern Ancheta <ancheta.wern@gmail.com>
2020-09-05 22:14:21 +0800

    transfer the questionnaire over to the qr code generation form

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -39,7 +39,122 @@
         <b-input type="text" v-model="user.address" required></b-input>
       </b-field>
 
-      <b-button class="is-info" @click="saveEntry" expanded>
+      <div class="block">
+        <b-field
+          label="Have you have worked, visited, transited in any foreign country in the past 14 days?"
+        />
+        <b-radio v-model="user.travelled_foreign" native-value="Y">
+          Yes
+        </b-radio>
+        <b-radio v-model="user.travelled_foreign" native-value="N">
+          No
+        </b-radio>
+      </div>
+
+      <div class="block">
+        <b-field
+          label="Cities in the Philippines you have worked, lived or transited in the past 14 days?"
+        />
+        <b-radio v-model="user.other_cities" native-value="Y">
+          Yes
+        </b-radio>
+        <b-radio v-model="user.other_cities" native-value="N">
+          No
+        </b-radio>
+      </div>
+
+      <div class="block">
+        <b-field label="Sore throat" />
+        <b-radio v-model="user.sore_throat" native-value="Y">
+          Yes
+        </b-radio>
+        <b-radio v-model="user.sore_throat" native-value="N">
+          No
+        </b-radio>
+      </div>
+
+      <div class="block">
+        <b-field label="Body pains" />
+        <b-radio v-model="user.body_pains" native-value="Y">
+          Yes
+        </b-radio>
+        <b-radio v-model="user.body_pains" native-value="N">
+          No
+        </b-radio>
+      </div>
+
+      <div class="block">
+        <b-field label="Headache" />
+        <b-radio v-model="user.headache" native-value="Y">
+          Yes
+        </b-radio>
+        <b-radio v-model="user.headache" native-value="N">
+          No
+        </b-radio>
+      </div>
+
+      <div class="block">
+        <b-field label="Fever" />
+        <b-radio v-model="user.fever" native-value="Y">
+          Yes
+        </b-radio>
+        <b-radio v-model="user.fever" native-value="N">
+          No
+        </b-radio>
+      </div>
+
+      <div class="block">
+        <b-field label="Have you been sick in the past 30 days?" />
+        <b-radio v-model="user.sick" native-value="Y">
+          Yes
+        </b-radio>
+        <b-radio v-model="user.sick" native-value="N">
+          No
+        </b-radio>
+      </div>
+
+      <div class="block">
+        <b-field
+          label="Have you had any contact with anyone with fever, cough, cold, and sore throat in the past 14 days?"
+        />
+        <b-radio v-model="user.close_contact_person" native-value="Y">
+          Yes
+        </b-radio>
+        <b-radio v-model="user.close_contact_person" native-value="N">
+          No
+        </b-radio>
+      </div>
+
+      <div class="block">
+        <b-field
+          label="In the last 14 days, have you been in close contact with farm animals or exposed to wild animals?"
+        />
+        <b-radio v-model="user.close_contact_animal" native-value="Y">
+          Yes
+        </b-radio>
+        <b-radio v-model="user.close_contact_animal" native-value="N">
+          No
+        </b-radio>
+      </div>
+
+      <div class="block">
+        <b-field
+          label="I understand that my personal information is protected by RA 10173, Data Privacy Act of 2012, and that I am required by RA 11469, Bayanihan to Heal as One Act, to provide thruthful information."
+        />
+        <b-radio v-model="agree" native-value="Y">
+          Yes
+        </b-radio>
+        <b-radio v-model="agree" native-value="N">
+          No
+        </b-radio>
+      </div>
+
+      <b-button
+        class="is-info"
+        @click="saveEntry"
+        expanded
+        :disabled="isGenerateQRDisabled"
+      >
         Generate QR Code
       </b-button>
     </div>
@@ -81,6 +196,12 @@ Vue.component(VueQrcode.name, VueQrcode);
 export default {
   name: "RegisterForm",
 
+  computed: {
+    isGenerateQRDisabled: function() {
+      return this.agree === "N";
+    },
+  },
+
   data() {
     return {
       showNotification: true,
@@ -93,7 +214,23 @@ export default {
         phone: "",
         address: "",
         gender: "M",
+
+        // questions
+        travelled_foreign: "N",
+
+        other_cities: "N",
+
+        sore_throat: "N",
+        body_pains: "N",
+        headache: "N",
+        fever: "N",
+        sick: "N",
+
+        close_contact_person: "N",
+
+        close_contact_animal: "N",
       },
+      agree: "N",
       url: "",
     };
   },
@@ -106,6 +243,7 @@ export default {
     saveEntry() {
       const user = JSON.stringify(this.user);
       const cipherText = this.encrypt(user);
+
       this.url = cipherText;
 
       Vue.localStorage.set("user", user);
--- src/components/ScanForm.vue
+++ src/components/ScanForm.vue
@@ -6,12 +6,6 @@
       <b-table :data="[visitor]" :columns="columns"></b-table>
     </div>
 
-    <div class="boxu">
-      <b-button class="is-info" @click="saveAndSkip" expanded>
-        Save Entry and Skip Questionnaire
-      </b-button>
-    </div>
-
     <div class="form-container">
       <b-field label="Date">
         <b-datepicker
@@ -37,123 +31,12 @@
         <b-input type="text" v-model="questions.temperature"></b-input>
       </b-field>
 
-      <div class="block">
-        <b-field
-          label="Have you have worked, visited, transited in any foreign country in the past 14 days?"
-        />
-        <b-radio
-          v-model="questions.travelled_foreign_countries"
-          native-value="yes"
-        >
-          Yes
-        </b-radio>
-        <b-radio
-          v-model="questions.travelled_foreign_countries"
-          native-value="no"
-        >
-          No
-        </b-radio>
-      </div>
-
-      <div class="block">
-        <b-field
-          label="Cities in the Philippines you have worked, lived or transited in the past 14 days?"
-        />
-        <b-radio v-model="questions.cities_worked" native-value="yes">
-          Yes
-        </b-radio>
-        <b-radio v-model="questions.cities_worked" native-value="no">
-          No
-        </b-radio>
-      </div>
-
-      <div class="block">
-        <b-field label="Sore throat" />
-        <b-radio v-model="questions.symptoms_sore_throat" native-value="yes">
-          Yes
-        </b-radio>
-        <b-radio v-model="questions.symptoms_sore_throat" native-value="no">
-          No
-        </b-radio>
-      </div>
-
-      <div class="block">
-        <b-field label="Body pains" />
-        <b-radio v-model="questions.symptoms_body_pains" native-value="yes">
-          Yes
-        </b-radio>
-        <b-radio v-model="questions.symptoms_body_pains" native-value="no">
-          No
-        </b-radio>
-      </div>
-
-      <div class="block">
-        <b-field label="Headache" />
-        <b-radio v-model="questions.symptoms_headache" native-value="yes">
-          Yes
-        </b-radio>
-        <b-radio v-model="questions.symptoms_headache" native-value="no">
-          No
-        </b-radio>
-      </div>
-
-      <div class="block">
-        <b-field label="Fever" />
-        <b-radio v-model="questions.symptoms_fever" native-value="yes">
-          Yes
-        </b-radio>
-        <b-radio v-model="questions.symptoms_fever" native-value="no">
-          No
-        </b-radio>
-      </div>
-
-      <div class="block">
-        <b-field label="Have you been sick in the past 30 days?" />
-        <b-radio v-model="questions.sick" native-value="yes">
-          Yes
-        </b-radio>
-        <b-radio v-model="questions.sick" native-value="no">
-          No
-        </b-radio>
-      </div>
-
-      <div class="block">
-        <b-field
-          label="Have you had any contact with anyone with fever, cough, cold, and sore throat in the past 14 days?"
-        />
-        <b-radio v-model="questions.close_contact_person" native-value="yes">
-          Yes
-        </b-radio>
-        <b-radio v-model="questions.close_contact_person" native-value="no">
-          No
-        </b-radio>
-      </div>
-
-      <div class="block">
-        <b-field
-          label="In the last 14 days, have you been in close contact with farm animals or exposed to wild animals?"
-        />
-        <b-radio v-model="questions.close_contact_animal" native-value="yes">
-          Yes
-        </b-radio>
-        <b-radio v-model="questions.close_contact_animal" native-value="no">
-          No
-        </b-radio>
-      </div>
-
-      <div class="block">
-        <b-field
-          label="I understand that my personal information is protected by RA 10173, Data Privacy Act of 2012, and that I am required by RA 11469, Bayanihan to Heal as One Act, to provide thruthful information."
-        />
-        <b-radio v-model="questions.agree_to_collect_data" native-value="yes">
-          Yes
-        </b-radio>
-        <b-radio v-model="questions.agree_to_collect_data" native-value="no">
-          No
-        </b-radio>
-      </div>
-
-      <b-button class="is-info" @click="saveEntry" expanded>
+      <b-button
+        class="is-info"
+        @click="saveEntry"
+        expanded
+        :disabled="visitor === null"
+      >
         Save Entry
       </b-button>
     </div>
@@ -172,6 +55,7 @@ export default {
   components: {
     QrcodeStream,
   },
+
   data() {
     return {
       visitor: null,
@@ -183,22 +67,6 @@ export default {
         date: new Date(),
         time: new Date(),
         temperature: "36",
-
-        travelled_foreign_countries: "no",
-
-        cities_worked: "no",
-
-        symptoms_sore_throat: "no",
-        symptoms_body_pains: "no",
-        symptoms_headache: "no",
-        symptoms_fever: "no",
-        sick: "no",
-
-        close_contact_person: "no",
-
-        close_contact_animal: "no",
-
-        agree_to_collect_data: "yes",
       },
 
       columns: [
@@ -230,29 +98,75 @@ export default {
           field: "gender",
           label: "Gender",
         },
+
+        {
+          field: "travelled_foreign",
+          label: "Travelled to foreign country",
+        },
+        {
+          field: "other_cities",
+          label: "Visited other cities",
+        },
+        {
+          field: "sore_throat",
+          label: "Has sore throat",
+        },
+        {
+          field: "body_pains",
+          label: "Has body pains",
+        },
+        {
+          field: "headache",
+          label: "Has headache",
+        },
+        {
+          field: "fever",
+          label: "Has fever",
+        },
+        {
+          field: "sick",
+          label: "Has been sick",
+        },
+        {
+          field: "close_contact_person",
+          label: "Close contact person",
+        },
+        {
+          field: "close_contact_animal",
+          label: "Close contact animal",
+        },
       ],
     };
   },
   methods: {
     onDecode(decodedString) {
-      const encodedWord = CryptoJS.enc.Base64.parse(decodedString);
-      const decoded = CryptoJS.enc.Utf8.stringify(encodedWord);
+      try {
+        const encodedWord = CryptoJS.enc.Base64.parse(decodedString);
+        const decoded = CryptoJS.enc.Utf8.stringify(encodedWord);
 
-      this.visitor = JSON.parse(decoded);
+        this.visitor = JSON.parse(decoded);
+      } catch (err) {
+        console.log("err occurred: ", err);
+      }
     },
 
     saveVisitor() {
       if (this.visitor) {
-        this.questions.date = formatDate("yyyy/MM/dd", this.questions.date);
-        this.questions.time = formatDate("hh:mm", this.questions.time);
+        try {
+          Object.assign(this.visitor, {
+            temperature: this.questions.temperature,
+            date: formatDate("yyyy/MM/dd", this.questions.date),
+            time: formatDate("hh:mm", this.questions.time),
+          });
 
-        Object.assign(this.visitor, this.questions);
+          EventBus.$emit("visitor:add", this.visitor);
 
-        EventBus.$emit("visitor:add", this.visitor);
-
-        this.$buefy.dialog.alert("Visitor added on file!");
-        this.resetForm();
-        window.scrollTo(0, 0);
+          this.$buefy.dialog.alert("Visitor added on file!");
+          this.resetForm();
+          window.scrollTo(0, 0);
+        } catch (err) {
+          console.log("err: ", err);
+        }
       } else {
         this.$buefy.dialog.alert("You forgot to scan first.");
       }
@@ -268,30 +182,11 @@ export default {
 
     resetForm() {
       this.visitor = null;
+
       this.questions = {
-        travelled_foreign_countries: "no",
-        foreign_countries_text: "",
-
-        cities_worked: "no",
-
-        symptoms_sore_throat: "no",
-        symptoms_body_pains: "no",
-        symptoms_headache: "no",
-        symptoms_fever: "no",
-        sick: "no",
-
-        hospitals_visited: "no",
-        hospitals_visited_text: "",
-
-        symptoms: "no",
-        symptoms_text: "",
-
-        close_contact_person: "no",
-
-        close_contact_animal: "no",
-        close_contact_animal_text: "",
-
-        agree_to_collect_data: "yes",
+        date: new Date(),
+        time: new Date(),
+        temperature: "36",
       };
     },
   },
